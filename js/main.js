f5f8dd7a9091e0722f8ebd9b268271f24d8c6a5f
Vladimir Medvedev <vladmirworld@gmail.com>
2020-02-20 14:43:54 +0300

    lesson2

--- js/main.js
+++ js/main.js
@@ -1,49 +1,104 @@
-/*
-1. Добавьте стили для верхнего меню, товара, списка товаров и кнопки вызова корзины.
-2. Добавьте значения по умолчанию для аргументов функции. Как можно упростить или сократить запись функций?
-3. *Сейчас после каждого товара на странице выводится запятая. Из-за чего это происходит? Как это исправить?
- */
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
-
-const renderProduct = (title, price, image = 'https://via.placeholder.com/150C/O https://placeholder.com/') => {  /// добавлена картинка и заглушка
-    return `<div class="products__item">
-                <a href="#"><img src="${image}" alt="#" class="products__img"></a>
-                <h3 class="products__h">${title}</h3>
-                <p class="products__p">${price}</p>
-                <button class="products__btn">Добавить в корзину</button>
-            </div>`;
-};
-
-
-//////////////////////////1) Упрощенная версия  ////////////////////////////////////////////////////////////////////
-/*
-const renderProducts = (list) => {
-    return document.querySelector('.products').innerHTML = list.map((element) => renderProduct(element.title, element.price));
-};
-*/
-
-
-/////////////////////////// 3) Больше нет запятых, так передеатся не весь массив и не создается новый, а берется только объект по индексу /////////////////////
-/*
-const renderProducts = (list) => {
-    for (index = 0; index < list.length; index++) {
-        document.querySelector('.products').innerHTML += renderProduct(list[index].title, list[index].price);
+class ProductItem {
+    constructor(product, image = 'https://via.placeholder.com/150C/O https://placeholder.com/') {
+        this.image = image;
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
     }
-};
- */
 
-/// в ДЗ написано "нопки вызова корзины." так что сделал и переменную с кнопкой, и сделал ее кновпой вызова товара
-let button = document.querySelector('.cart-section__button');
+    render() {
+        return `<div class="products__item" data-id='${this.id}'>
+                <a href="#"><img src="${this.image}" alt="#" class="products__img"></a>
+                <h3 class="products__h">${this.title}</h3>
+                <p class="products__p">${this.price}</p>
+                <button class="products__btn">Добавить в корзину</button>
+            </div>`
+    }
+
+
+}
+
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+        this.render();
+    }
+
+    _fetchProducts() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keyboard', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ];
+    }
+
+    calcTotalPrice() {
+        let totalPrice = 0;
+        for (let i = 0; i < this.goods.length; i++) {
+            totalPrice += this.goods[i].price;
+        }
+        return console.log(totalPrice);
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML("beforeend", productObject.render());
+        }
+    }
+
+
+}
+
+const product = new ProductList();
+product.calcTotalPrice();
+
+/*
+
+// при нажатии на 'добавить в корзину" дродукт добавляется в массив cartGoods
+let button = document.querySelector('.products__btn');
 
 button.addEventListener('click', () => {
-    for (let index = 0; index < products.length; index++) {
-        document.querySelector('.products').innerHTML += renderProduct(products[index].title, products[index].price);
-    }
+
 });
 
-//renderProducts(products);
+// добавление
+class CartList {
+    constructor(container='.cart-block', cartGoods) {
+        this.container = container;
+        this.cartGoods = cartGoods;
+        this.cartRender() {
+            const block = document.querySelector(this.container);
+
+            for (let product of cartGoods) {
+                 const cartProductObject = new ProductItem(product);
+                block.insertAdjacentHTML("beforeend", productObject.render());
+            }
+        }
+    }
+}
+
+ */
+/*
+//можно добавить форму в блоке продукта, чтобы пережавать сразу колличество
+class CartProductItem extends ProductItem{
+    constructor(product, image = 'https://via.placeholder.com/150C/O https://placeholder.com/', quantity) {
+        super(product, image = 'https://via.placeholder.com/150C/O https://placeholder.com/');
+        this.quanity = quantity;
+    }
+
+    delete() {
+        //метод удаления из карзины
+}
+    changeQuanity() {
+        //метод изменения колличества товара
+    }
+
+ */
